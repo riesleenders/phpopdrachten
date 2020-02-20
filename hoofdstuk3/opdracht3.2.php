87a10379e6f748d480e8f223fa7b3555793df2e5
Ries <ries.leenders@edu-kw1c.nl>
2020-02-20 09:50:38 +0100

    klaar

---
+++ hoofdstuk3/opdracht3.2.php
@@ -0,0 +1,179 @@
+<?php
+/**
+ * User: Ries Leenders
+ * Date: 20-2-2020
+ * Time: 8:42
+ * File: opdracht3.2.php
+ */
+?>
+    <!DOCTYPE HTML>
+    <html>
+    <head>
+        <meta name="description" content=" ">
+        <meta name="keywords" content=" ">
+        <meta charset="UTF-8">
+        <link href="/phpopdrachten/css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    <header>
+        <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+    </header>
+    <aside>
+        <section>
+            <h2>Menu</h2>
+            <ul>
+                <li>Hoofdstuk 2
+                    <ul>
+                        <li>
+                            <a href="opdracht2.1.php">Opdracht 2.1</a>
+                        </li>
+                        <li>
+                            <a href="opdracht2.2.php">Opdracht 2.2</a>
+                        </li>
+                    </ul>
+                </li>
+                <br>
+                <li>Hoofdstuk 3
+                    <ul>
+                        <li>
+                            <a href="hoofdstuk3/opdracht3.1.php">Opdracht 3.1</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk3/opdracht3.2.php">Opdracht 3.2</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk3/opdracht3.3.php">Opdracht 3.3</a>
+                        </li>
+                    </ul>
+                </li>
+                <br>
+                <li>Hoofdstuk 4
+                    <ul>
+                        <li>
+                            <a href="hoofdstuk4/opdracht4.1.php">Opdracht 4.1</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk4/opdracht4.2.php">Opdracht 4.2</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk4/opdracht4.3.php">Opdracht 4.3</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk4/opdracht4.4.php">Opdracht 4.4</a>
+                        </li>
+                    </ul>
+                </li>
+                <br>
+                <li>Hoofdstuk 5
+                    <ul>
+                        <li>
+                            <a href="hoofdstuk5/opdracht5.1.php">Opdracht 5.1</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk5/opdracht5.2.php">Opdracht 5.2</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk5/opdracht5.3.php">Opdracht 5.3</a>
+                        </li>
+                        <li>
+                            <a href="hoofdstuk5/opdracht5.4.php">Opdracht 5.4</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <br>
+            <a href="/phpopdrachten/index.php">Terug</a>
+        </section>
+    </aside>
+    <main id="wrapper">
+        <h2>Uitwerkingen</h2>
+        <?php
+            // Declareren en initialiseren van de gewenste variabelen
+            $trafficLightColor = "Groen";
+            $ambulanceComing = true;
+
+            // driveOn declareren en initialiseren
+            $driveOn = true;
+
+            if($trafficLightColor == "Groen" && $ambulanceComing == false)
+            {
+                $driveOn = true;
+            }
+            else if($trafficLightColor == "Groen" && $ambulanceComing == true)
+            {
+                $driveOn = false;
+            }
+            else if($trafficLightColor == "Oranje")
+            {
+                $driveOn = false;
+            }
+            else if($trafficLightColor == "Rood")
+            {
+                $driveOn = false;
+            }
+            else
+            {
+                $driveOn = true;
+                echo("<p>foutje. Het stoplicht heeft een ongeldige kleur.</p>");
+            }
+
+            // Tonen van melding aan de automobilist
+            if($driveOn == true)
+            {
+                echo("<h1 class ='Greentext'>U mag doorrijden.</h1>");
+            }
+            else
+            {
+                echo("<h1 style='color: red;'>U mag niet doorrijden.");
+            }
+        ?>
+
+        <hr>
+
+        <?php
+
+            // Deel 2: Alcohol in verschillende landen opdracht
+            $countryName = "Nederland";
+            $currentAge = 16;
+
+            // Tonen algemene gegevens drank
+            echo("<p>Je woont in ". $countryName . " en je bent " . $currentAge . " jaar oud.</p>");
+
+            // Tonen van de drank informatie België
+            if($countryName == "België" && $currentAge >= 16 && $currentAge <= 18)
+            {
+                echo("<p>Je mag hier zwakke alcohol drinken.</p>");
+            }
+            else if($countryName == "België" && $currentAge >= 18)
+            {
+                echo("<p>Je mag hier sterke alcohol drinken.</p>");
+            }
+
+            // Tonen van de drank informatie Bulgarijë
+            if($countryName == "Bulgarije" && $currentAge >= 18)
+            {
+                echo("<p>Je mag hier sterke en zwakke alcohol drinken.</p>");
+            }
+            else
+            {
+                echo("<p>Hier mag je geen alcohol drinken.</p>");
+            }
+
+            // Tonen van de drank informatie
+            if($countryName == "België" && $currentAge >= 16 && $currentAge <= 18)
+            {
+                echo("<p>Je mag hier zwakke alcohol drinken.</p>");
+            }
+            else if($countryName == "België" && $currentAge >= 18)
+            {
+                echo("<p>Je mag hier sterke alcohol drinken.</p>");
+            }
+
+
+        ?>
+    </main>
+    </body>
+    </html>
+
+
+<?php
