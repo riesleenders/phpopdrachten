b70e96db9c108203084a3ffc6633fe4fc7d2ffc5
Ries <ries.leenders@edu-kw1c.nl>
2020-03-03 12:00:49 +0100

    klaar

---
+++ hoofdstuk4/instructie 1 - Timezone.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * User: Ries Leenders
+ * Date: 20-2-2020
+ * Time: 8:42
+ * File: opdracht3.2.php
+ */
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+
+</head>
+<body>
+
+<?php
+    // Instructie 1 Hoofdstuk 4
+
+    // Vertellen dat ons script de tijdzones van Hong Kong moet aanhouden.
+    date_default_timezone_set("Asia/Hong_Kong");
+
+    // Toon de tijd in HH:MM formaat
+    $currentDate = date("D d m Y H:i:s");
+    echo("<p>Huidige Tijd: " . $currentDate . "</p>");
+
+    // Oefening 2: switch
+    $currentHour = date("D");
+    $name = "Ries";
+
+    switch($currentHour)
+    {
+        case "Mon":
+            echo("Vandaag is het de eerste dag van de week.");
+            break;
+        case "Tue":
+            echo("Vandaag is het de tweede dag van de week.");
+            break;
+        case "Wed":
+            echo("Vandaag is het de derde dag van de week.");
+            break;
+        case "Thu":
+            echo("Vandaag is het de vierde dag van de week.");
+            break;
+        case "Fri":
+            echo("Vandaag is het de vijfde dag van de week.");
+            break;
+        case "Sat":
+            echo("Vandaag is het de zesde dag van de week.");
+                if($name == "Ries")
+                {
+                    echo("Vandaag week lekker werken en geld verdienen.");
+                }
+                else
+                {
+                    echo("Slaap maar lekker uit.");
+                }
+            break;
+        case "Sun":
+            echo("Vandaag is het de zevende en laatste dag van de week.");
+            break;
+    }
+?>
+
+
+
+
+</body>
+</html>
